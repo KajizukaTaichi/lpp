6b4efca12b40f0d680c130f548e173d0b33eada5
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-01 23:48:08 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 use lambda::Lambda;
 use lexer::tokenize;
+use std::env::args;
 use std::fmt::{self, Display, Formatter};
+use std::process::Command;
 
 mod lambda;
 mod lexer;
 mod lpp;
 
+const CMD: &str = "python3";
+
 fn main() {
-    let expr = lpp::Expr::parse("1 + 2 + 2 * 3")
-        .unwrap()
-        .compile()
-        .unwrap()
-        .compile();
-    println!("{expr}(lambda x: x + 1)(0)");
+    let expr = args().collect::<Vec<_>>()[1..].join(" ");
+    let expr = lpp::Expr::parse(&expr).unwrap().compile().unwrap();
+    println!("Lambda Calculus Formula: {expr}");
+
+    let pycode = format!("print({}(lambda x: x + 1)(0))", expr.compile());
+    let output = Command::new(CMD).args(["-c", &pycode]).output().unwrap();
+    let output = String::from_utf8_lossy(&output.stdout).trim().to_string();
+    println!("Evaluated Result: {output}");
 }
